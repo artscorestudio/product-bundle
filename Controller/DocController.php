4f0fe9916e77c0b7a974f9e47604836d6c040c57
Nicolas Claverie <info@artscore-studio.fr>
2016-03-10 12:53:49 +0100

    Create Doc controller for online doc in app

---
+++ Controller/DocController.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the Artscore Studio Framework package.
+ *
+ * (c) Nicolas Claverie <info@artscore-studio.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ASF\ProductBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Documentation Controller
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class DocController extends Controller
+{
+    /**
+     * Documentation Homepage
+     * 
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('ASFProductBundle:Doc:index.html.twig');
+    }
+}
