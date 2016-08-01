fd03d89140808fd6076aaf78e9ca9e6a3cb11322
Nicolas Claverie <info@artscore-studio.fr>
2016-08-01 16:36:33 +0200

    Update repository methods for Category.

--- Repository/CategoryRepository.php
+++ Repository/CategoryRepository.php
@@ -22,14 +22,50 @@ use Doctrine\ORM\Query\Expr;
 class CategoryRepository extends EntityRepository
 {
     /**
+     * Return a Query Builder with states filter.
+     *
+     * @param array|null $states
+     *
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    public function getQueryBuilder($states)
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb instanceof QueryBuilder;
+    
+        if ( null !== $states ) {
+            $qb->add('where', $qb->expr()->in('c.state', $states));
+        }
+    
+        return $qb;
+    }
+    
+    /**
+     * Find categories by exact name
+     * 
+     * @param unknown $searched_term
+     * @param array $states
+     * 
+     * @return mixed|NULL|\Doctrine\DBAL\Driver\Statement
+     */
+    public function findByName($searched_term, array $states = null)
+    {
+        $qb = $this->getQueryBuilder($states);
+    
+        $qb->add('where', $qb->expr()->like('c.name', $qb->expr()->lower(':searched_term')))
+            ->setParameter('searched_term', $searched_term);
+    
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+    
+    /**
      * Find categories by name.
      *
      * @param string $searched_term
      */
-    public function findByNameContains($searched_term)
+    public function findByNameContains($searched_term, array $states = null)
     {
-        $qb = $this->createQueryBuilder('b');
-        $qb instanceof QueryBuilder;
+        $qb = $this->getQueryBuilder($states);
 
         $qb->add('where', $qb->expr()->like('b.name', $qb->expr()->lower(':searched_term')))
             ->setParameter('searched_term', $searched_term.'%');
